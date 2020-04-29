2e3577f30b7e84b3548ee3bfa47334ced1a9951d
Yousef Osman <yousef.osman.31@gmail.com>
2020-04-29 16:09:16 +0200

    Add CRUD Operations to the api

--- ExpenseManagerAPI/Controllers/ExpensesController.cs
+++ ExpenseManagerAPI/Controllers/ExpensesController.cs
@@ -1,13 +1,16 @@
 ﻿using ExpenseManagerAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ExpenseManagerAPI.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class ExpensesController : ApiController
     {
         public IHttpActionResult GetExpenses()
@@ -24,6 +27,60 @@ namespace ExpenseManagerAPI.Controllers
 			{
                 return BadRequest(ex.Message);
 			}
+        }
+
+        public IHttpActionResult GetExpenses(int id)
+        {
+            try
+            {
+                using (ExpensesDbContext db = new ExpensesDbContext())
+                {
+                    var expense = db.Expenses.Where(e => e.Id == id).SingleOrDefault();
+                    var expense1 = db.Expenses.Find(id);
+                    return Ok(expense);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostExpenses(Expense exp)
+        {
+            try
+            {
+                using (ExpensesDbContext db = new ExpensesDbContext())
+                {
+                    db.Entry(exp).State = (exp.Id == 0) ? EntityState.Added : EntityState.Modified;
+                    db.SaveChanges();
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteExpenses(int id)
+        {
+            try
+            {
+                using (ExpensesDbContext db = new ExpensesDbContext())
+                {
+                    var exp = db.Expenses.Where(e => e.Id == id).SingleOrDefault();
+                    db.Entry(exp).State = EntityState.Deleted;
+                    db.SaveChanges();
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
